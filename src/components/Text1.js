4f1b945e93324b0cc4fdd43ddc595942b169299a
Yoshita Shaw <109474278+yoshitashaw@users.noreply.github.com>
2023-11-20 19:54:38 +0530

    Update Text1.js

--- src/components/Text1.js
+++ src/components/Text1.js
@@ -1,48 +1,29 @@
-import { css } from "@emotion/css";
+import styled from "styled-components";
 
-const LetUsHelp = () => {
+const Text1 = styled.p`
+  margin: 0;
+`;
+const Text1ContainerRoot = styled.div`
+  font-size: 24px;
+  line-height: 80px;
+  font-weight: 600;
+  font-family: var(--font-jost);
+  color: var(--color-white);
+  text-align: center;
+  display: inline-block;
+  width: 514px;
+  height: 414px;
+`;
+
+const Text1 = () => {
   return (
-    <div
-      className={css`
-        font-size: 24px;
-        line-height: 80px;
-        font-weight: 600;
-        font-family: var(--font-jost);
-        color: var(--color-white);
-        text-align: center;
-        display: inline-block;
-        width: 514px;
-        height: 414px;
-      `}
-    >
-      <p
-        className={css`
-          margin: 0;
-        `}
-      >{`Let us help you to find the best stay `}</p>
-      <p
-        className={css`
-          margin: 0;
-        `}
-      >
-        which is no less than a home ---
-      </p>
-      <p
-        className={css`
-          margin: 0;
-        `}
-      >
-        to the best of your budget, security and
-      </p>
-      <p
-        className={css`
-          margin: 0;
-        `}
-      >
-        comfort. Happy Stay!
-      </p>
-    </div>
+    <Text1ContainerRoot>
+      <Text1>{`Let us help you to find the best stay `}</LetUsHelp1>
+      <Text1>which is no less than a home ---</LetUsHelp1>
+      <Text1>to the best of your budget, security and</LetUsHelp1>
+      <Text1>comfort. Happy Stay!</LetUsHelp1>
+    </Text1ContainerRoot>
   );
 };
 
-export default LetUsHelp;
+export default Text1;
