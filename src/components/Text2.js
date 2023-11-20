f0222ea7b83b77dc870a9fc03ca864cdfe036a8f
Yoshita Shaw <109474278+yoshitashaw@users.noreply.github.com>
2023-11-20 19:57:53 +0530

    Update Text2.js

--- src/components/Text2.js
+++ src/components/Text2.js
@@ -1,40 +1,35 @@
-import { css } from "@emotion/css";
+import styled from "styled-components";
 
-const StudentHiveWhereLearning = () => {
+const Text2 = styled.p`
+  margin: 0;
+`;
+const Text2ContainerRoot = styled.div`
+  font-size: 26px;
+  line-height: 80px;
+  font-weight: 600;
+  font-family: var(--font-jost);
+  color: var(--color-white);
+  text-align: center;
+  display: inline-block;
+  width: 707px;
+  height: 533px;
+`;
+
+const Text2 = () => {
   return (
-    <div
-      className={css`
-        font-size: 26px;
-        line-height: 80px;
-        font-weight: 600;
-        font-family: var(--font-jost);
-        color: var(--color-white);
-        text-align: center;
-        display: inline-block;
-        width: 707px;
-        height: 533px;
-      `}
-    >
-      <p
-        className={css`
-          margin: 0;
-        `}
-      >
+    <Text2ContainerRoot>
+      <Text2>
         StudentHive: Where Learning Meets Living is a
-      </p>
-      <p
-        className={css`
-          margin: 0;
-        `}
-      >
+      </Text2>
+      <Text2>
         rental portal for the students who can easily find, compare, and rent
         accommodation for their studies. It's like an online hub where students
         can search for apartments or rooms near their universities, see photos
         and details, and connect with landlords or property managers to secure a
         place to live during their academic year.
-      </p>
-    </div>
+      </Text2>
+    </Text2ContainerRoot>
   );
 };
 
-export default StudentHiveWhereLearning;
+export default Text2;
